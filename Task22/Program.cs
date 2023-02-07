de48ac30adb055ea9b8a4cdca7606ad5690e9737
Maxim Prav <mxmlknv@mail.ru>
2023-02-07 17:02:38 +0300

    Задача с квадратом числа и счетчиком

---
+++ Task22/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+Console.Clear();
+Console.WriteLine("Введите любое положительное число:");
+int N = int.Parse(Console.ReadLine());
+int count = 1;
+
+if (N<1)
+{
+    Console.WriteLine("Задано неверное число.");
+}
+else
+{
+    while (count < N)
+    {
+        Console.Write ($"{count*count}, ");
+        count++;
+    }
+Console.Write($"{N*N}.");
+}
